6e49c878f07f0874d8a887465bc472e75dff2536
vogje01 <vogje01@hlag.com>
2020-05-23 19:00:47 +0200

    Check authentication.

--- batch-manager/src/main/java/com/hlag/fis/batch/manager/controller/LoginController.java
+++ batch-manager/src/main/java/com/hlag/fis/batch/manager/controller/LoginController.java
@@ -31,7 +31,7 @@ import static java.text.MessageFormat.format;
  * @since 0.0.1
  */
 @RestController
-@CrossOrigin(origins = {"http://localhost:3000", "http://localhost"}, allowCredentials = "true")
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost", "http://localhost:8080"}, allowCredentials = "true")
 public class LoginController {
 
     private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
