bf33d322a8699d187b0912a9936254e7f966158e
vogje01 <vogje01@hlag.com>
2020-05-24 17:49:56 +0200

    Remove HATEOAS from batch-common.

--- batch-common/src/main/java/com/hlag/fis/batch/repository/JobGroupRepository.java
+++ batch-common/src/main/java/com/hlag/fis/batch/repository/JobGroupRepository.java
@@ -6,11 +6,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Optional;
 
-@RepositoryRestResource(collectionResourceRel = "jobgroups", path = "jobgroups")
 public interface JobGroupRepository extends PagingAndSortingRepository<JobGroup, String> {
 
     @Query("select j from JobGroup j where j.active = true")
