b3daf87b23a1afb10ef46bfc1c7dc173b0d18334
vogje01 <vogje01@hlag.com>
2020-06-06 22:33:29 +0200

    Fix job execution log repo.

--- batch-server/batch-database/src/main/java/com/momentum/batch/server/database/repository/JobExecutionLogRepository.java
+++ batch-server/batch-database/src/main/java/com/momentum/batch/server/database/repository/JobExecutionLogRepository.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface JobExecutionLogRepository extends PagingAndSortingRepository<JobExecutionLog, String> {
 
-    @Query("select count(j) from JobExecutionLog j where j.instant.epochSecond < :cutOffDate")
+    @Query("select count(j) from JobExecutionLog j where j.timestamp < :cutOffDate")
     long countByTimestamp(@Param("cutOffDate") long cutOffDate);
 
     @Query("select count(j) from JobExecutionLog j where j.jobUuid = :jobId and j.stepUuid is null")
