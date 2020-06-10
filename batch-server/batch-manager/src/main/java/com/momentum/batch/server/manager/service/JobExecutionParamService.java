03c344d1b6c5dcfd56b9e66309af7f7de795c7b2
vogje01 <vogje01@hlag.com>
2020-06-10 21:15:57 +0200

    Fix job name issue.

--- batch-server/batch-manager/src/main/java/com/momentum/batch/server/manager/service/JobExecutionParamService.java
+++ batch-server/batch-manager/src/main/java/com/momentum/batch/server/manager/service/JobExecutionParamService.java
@@ -1,26 +1,18 @@
 package com.momentum.batch.server.manager.service;
 
 import com.momentum.batch.server.database.domain.JobExecutionParam;
-import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
-@CacheConfig(cacheNames = "JobExecutionParam")
 public interface JobExecutionParamService {
 
-    @Cacheable
-    Page<JobExecutionParam> byJobId(String jobId, Pageable pageable);
+    Page<JobExecutionParam> findAll(Pageable pageable);
 
-    @Cacheable
-    Optional<JobExecutionParam> byId(String paramId);
+    Page<JobExecutionParam> findByJobId(String jobId, Pageable pageable);
 
-    @CacheEvict
+    Optional<JobExecutionParam> findById(String paramId);
+
     void deleteById(String logId);
-
-    @Cacheable
-    long countByJobExecutionId(String jobExecutionId);
 }
