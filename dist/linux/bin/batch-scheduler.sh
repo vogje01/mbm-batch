66bf1d7154aee68f212ec4e378e8c81f3d08d333
vogje01 <vogje01@hlag.com>
2020-06-14 11:59:11 +0200

    Formatting startup scripts.

--- dist/linux/bin/batch-scheduler.sh
+++ dist/linux/bin/batch-scheduler.sh
@@ -43,40 +43,40 @@ JAVA_ARGUMENTS="-Dscheduler.loggingDirectory=$LOG_DIR -Dlog4j2.configurationFile
 cd $BASEDIR
 case $1 in 
     start)
-       echo "Starting $SERVICE_NAME ..."
-       if [ ! -f $PID_PATH_NAME ]; then 
-	   nohup $JAVA $JAVA_ARGUMENTS -jar $LIB_DIR/$EXECUTABLE > $LOG_DIR/$MODULE.log 2>&1 &
-	   echo $! > $PID_PATH_NAME  
-	   echo "$SERVICE_NAME started ..."         
-       else 
-	   echo "$SERVICE_NAME is already running ..."
-       fi
-       ;;
+      echo "Starting $SERVICE_NAME ..."
+      if [ ! -f $PID_PATH_NAME ]; then
+	      nohup $JAVA $JAVA_ARGUMENTS -jar $LIB_DIR/$EXECUTABLE > $LOG_DIR/$MODULE.log 2>&1 &
+	      echo $! > $PID_PATH_NAME
+	      echo "$SERVICE_NAME started ..."
+      else
+	      echo "$SERVICE_NAME is already running ..."
+      fi
+      ;;
     stop)
-	if [ -f $PID_PATH_NAME ]; then
-            PID=$(cat $PID_PATH_NAME);
-            echo "$SERVICE_NAME stopping ..." 
-            kill $PID;         
-            echo "$SERVICE_NAME stopped ..." 
-            rm $PID_PATH_NAME       
-	else          
-            echo "$SERVICE_NAME is not running ..."   
-	fi    
-	;;    
+	    if [ -f $PID_PATH_NAME ]; then
+        PID=$(cat $PID_PATH_NAME);
+        echo "$SERVICE_NAME stopping ..."
+        kill $PID;
+        echo "$SERVICE_NAME stopped ..."
+        rm $PID_PATH_NAME
+	    else
+        echo "$SERVICE_NAME is not running ..."
+	    fi
+	    ;;
     restart)  
-	if [ -f $PID_PATH_NAME ]; then 
-	    PID=$(cat $PID_PATH_NAME);    
-	    echo "$SERVICE_NAME stopping ..."; 
-	    kill $PID;           
-	    echo "$SERVICE_NAME stopped ...";  
-	    rm $PID_PATH_NAME     
-	    echo "$SERVICE_NAME starting ..."  
-	    nohup $JAVA $JAVA_ARGUMENTS -jar $LIB_DIR/$EXECUTABLE > $LOG_DIR/$MODULE.log 2>&1 &
-	    echo $! > $PID_PATH_NAME  
-	    echo "$SERVICE_NAME started ..."    
-	else           
-	    echo "$SERVICE_NAME is not running ..."    
-	fi
-	;;
+	    if [ -f $PID_PATH_NAME ]; then
+	      PID=$(cat $PID_PATH_NAME);
+	      echo "$SERVICE_NAME stopping ...";
+	      kill $PID;
+	      echo "$SERVICE_NAME stopped ...";
+	      rm $PID_PATH_NAME
+	      echo "$SERVICE_NAME starting ..."
+	      nohup $JAVA $JAVA_ARGUMENTS -jar $LIB_DIR/$EXECUTABLE > $LOG_DIR/$MODULE.log 2>&1 &
+	      echo $! > $PID_PATH_NAME
+	      echo "$SERVICE_NAME started ..."
+	    else
+	      echo "$SERVICE_NAME is not running ..."
+	    fi
+	    ;;
 esac
 
