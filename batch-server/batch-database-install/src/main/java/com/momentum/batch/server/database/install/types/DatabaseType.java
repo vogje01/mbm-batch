2b45e96d7bf7831d6555c5b1a9235b25f3e3a7fd
vogje01 <vogje01@hlag.com>
2020-06-15 12:14:29 +0200

    Adding database management.

---
+++ batch-server/batch-database-install/src/main/java/com/momentum/batch/server/database/install/types/DatabaseType.java
@@ -0,0 +1,14 @@
+package com.momentum.batch.server.database.install.types;
+
+/**
+ * Supported database types.
+ *
+ * @author Jens Vogt (jensvogt47@gmail.com)
+ * @version 0.0.4
+ * @since 0.0.4
+ */
+public enum DatabaseType {
+
+    MYSQL,
+    ORACLE;
+}
