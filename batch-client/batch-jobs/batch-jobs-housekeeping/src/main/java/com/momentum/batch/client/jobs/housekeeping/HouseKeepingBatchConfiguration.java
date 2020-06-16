c193c3bb4f7956933d2914cd715225bccc3a6ca0
vogje01 <vogje01@hlag.com>
2020-06-16 17:05:03 +0200

    Cleanup batch jobs

--- batch-client/batch-jobs/batch-jobs-housekeeping/src/main/java/com/momentum/batch/client/jobs/housekeeping/HouseKeepingBatchConfiguration.java
+++ batch-client/batch-jobs/batch-jobs-housekeeping/src/main/java/com/momentum/batch/client/jobs/housekeeping/HouseKeepingBatchConfiguration.java
@@ -1,43 +1,55 @@
 package com.momentum.batch.client.jobs.housekeeping;
 
-import org.hibernate.SessionFactory;
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.launch.support.SimpleJobLauncher;
-import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.repository.support.MapJobRepositoryFactoryBean;
-import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import javax.sql.DataSource;
 import java.util.Objects;
-import java.util.Properties;
 
+/**
+ * Batch management housekeeping batch job.
+ *
+ * @author Jens Vogt (jensvogt47@gmail.com)
+ * @version 0.0.4
+ * @since 0.0.1
+ */
 @Configuration
 @EnableCaching
 @EnableBatchProcessing
+@EnableEncryptableProperties
 @EnableJpaRepositories(basePackages = {"com.momentum.batch.server.database.repository"})
-public class HouseKeepingBatchConfiguration {
+@EntityScan(basePackages = {"com.momentum.batch.server.database.domain"})
+public class HouseKeepingBatchConfiguration extends DefaultBatchConfigurer {
 
+    /**
+     * Class path resources
+     */
     private static final ClassPathResource[] configFiles = new ClassPathResource[]{new ClassPathResource("application.yml"), new ClassPathResource("houseKeeping.yml")};
 
+    /**
+     * Set job name.
+     *
+     * @return job name.
+     */
     @Bean
     String jobName() {
         return "Housekeeping Batch";
     }
 
+    /**
+     * Sets the property sources.
+     *
+     * @return property source configurer.
+     */
     @Bean
     public static PropertySourcesPlaceholderConfigurer properties() {
         PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
@@ -47,64 +59,14 @@ public class HouseKeepingBatchConfiguration {
         return configurer;
     }
 
-    @Bean
-    @ConfigurationProperties(prefix = "mysql.datasource")
-    public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
-    }
+    /**
+     * Overriding in order not to set datasource even if a datasource exist. Initialize will use a
+     * Map based JobRepository (instead of a database).
+     *
+     * @param dataSource batch data source.
+     */
+    @Override
+    public void setDataSource(DataSource dataSource) {
 
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        final LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(dataSource());
-        em.setPersistenceUnitName("MysqlPU");
-        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-        em.setJpaProperties(hibernateProperties());
-        em.setPackagesToScan("com.momentum.batch.server.database");
-        return em;
-    }
-
-    final Properties hibernateProperties() {
-        final Properties hibernateProperties = new Properties();
-        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
-        hibernateProperties.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
-        hibernateProperties.setProperty("hibernate.show_sql", "false");
-        return hibernateProperties;
-    }
-
-    public SessionFactory sessionFactory() {
-        return Objects.requireNonNull(entityManagerFactory().getObject()).unwrap(SessionFactory.class);
-    }
-
-    @Bean
-    public HibernateTransactionManager transactionManager() {
-        return new HibernateTransactionManager(sessionFactory());
-    }
-
-    @Bean
-    public JobRepository jobRepository() {
-        MapJobRepositoryFactoryBean factoryBean = new MapJobRepositoryFactoryBean(new ResourcelessTransactionManager());
-        try {
-            return factoryBean.getObject();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    @Bean
-    public JobLauncher jobLauncher(JobRepository jobRepository) {
-        SimpleJobLauncher jobLauncher = new SimpleJobLauncher();
-        jobLauncher.setJobRepository(jobRepository);
-        return jobLauncher;
-    }
-
-    @Bean
-    public ThreadPoolTaskExecutor getTaskExecutor() {
-        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
-        taskExecutor.setCorePoolSize(10);
-        taskExecutor.setMaxPoolSize(20);
-        taskExecutor.setQueueCapacity(30);
-        return taskExecutor;
     }
 }
