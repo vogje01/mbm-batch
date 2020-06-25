462aef51ba05669d9b2f23ff6459d120805cffc4
vogje01 <vogje01@hlag.com>
2020-06-25 13:47:06 +0100

    Deployment to github.

--- batch-server/batch-listener/src/main/java/com/momentum/batch/server/listener/BatchListenerApplication.java
+++ batch-server/batch-listener/src/main/java/com/momentum/batch/server/listener/BatchListenerApplication.java
@@ -3,6 +3,13 @@ package com.momentum.batch.server.listener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main application
+ *
+ * @author Jens Vogt (jensvogt47@gmail.com)
+ * @version 0.0.6-RELEASE
+ * @since 0.0.1
+ */
 @SpringBootApplication(scanBasePackages = {"com.momentum.batch.common, com.momentum.batch.server"})
 public class BatchListenerApplication {
 
